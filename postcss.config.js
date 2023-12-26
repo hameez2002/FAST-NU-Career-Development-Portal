29fe755fa6734760b569e3ac8305b875b866a193
Hameez Ahmed <hameezahmed23@gmail.com>
2023-12-27 00:50:10 +0500

    Failed build 2 errors resolved

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+// postcss.config.js
 module.exports = {
     plugins: {
       'postcss-import': {},
-      'tailwindcss/nesting': 'postcss-nesting', // Use postcss-nesting
-      tailwindcss: {},
-      autoprefixer: {},
+      'postcss-nesting': {}, // Enable postcss-nesting
+      'tailwindcss': {},
+      'autoprefixer': {},
     }
-  }
+  };
+  
